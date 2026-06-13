c197002ac32d2472e394bc5725aec9cdf1e87c10
Rohangambig <120889961+Rohangambig@users.noreply.github.com>
2026-06-13 09:37:57 +0530

    included login functionality

--- src/main/java/com/entire/demo/repository/userRepo.java
+++ src/main/java/com/entire/demo/repository/userRepo.java
@@ -2,6 +2,10 @@ package com.entire.demo.repository;
 
 import com.entire.demo.model.userModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface userRepo extends JpaRepository<userModel,Long> {
+    @Query("SELECT u FROM userModel u where u.email= :email")
+    userModel findByEmail(@Param("email") String email);
 }
