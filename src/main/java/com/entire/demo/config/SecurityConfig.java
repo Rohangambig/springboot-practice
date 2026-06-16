8a48aa01d617057c8522873eac1d28c7452ab4e9
Rohangambig <120889961+Rohangambig@users.noreply.github.com>
2026-06-16 07:38:59 +0530

    Revert the "updated configuration file"
    This reverts commit d6014ca995ab6aa6565247046602d82f29e6334c.

--- src/main/java/com/entire/demo/config/SecurityConfig.java
+++ src/main/java/com/entire/demo/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.entire.demo.config;
 
-import com.entire.demo.lib.JwtImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -17,14 +16,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(customize ->
-                customize.requestMatchers("/login","/register").permitAll()
-                        .anyRequest().authenticated()
-        );
-
+//        http.authorizeHttpRequests(customize -> customize.anyRequest().authenticated());
         http.httpBasic(Customizer.withDefaults());
+//        http.formLogin(Customizer.withDefaults());
         http.sessionManagement(customize -> customize.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.addFilterBefore(JwtImpl);
         return http.build();
     }
 
